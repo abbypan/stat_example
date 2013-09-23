a7da55794ec9346e5a2676fe1aff267b5cfd9ddd
abbypan <abbypan@gmail.com>
2013-09-23 21:21:00 +0800

    fix prov rank

--- prov_rank.r
+++ prov_rank.r
@@ -8,15 +8,15 @@ d <- read.table('prov_rank.csv', header=TRUE, sep=',')
 
 #省内排名，省内占比
 p <- ddply(d, .(state,prov), transform,
-        prov_rank = decr_rank(value), prov_rate = value/sum(value))
+        prov_rank = decr_rank(cnt), prov_rate = cnt/sum(cnt))
 
 #国内排名，国内占比
 s <- ddply(p, .(state), transform,
-        state_rank = decr_rank(value), state_rate = value/sum(value))
+        state_rank = decr_rank(cnt), state_rate = cnt/sum(cnt))
 
 #按用户数降序排列
-r <- s[order(s$value,decreasing=TRUE), ]
-names(r) <- c('object','state','prov','area','value',
+r <- s[order(s$cnt,decreasing=TRUE), ]
+names(r) <- c('object','state','prov','area','cnt',
                         'prov_rank','prov_rate','state_rank','state_rate');                                                                  
 #最终数据
-write.table(r, file='prov_rank_stat.csv', quote=FALSE, sep=',')
+write.table(r, file='prov_rank_stat.csv', sep=',', quote=FALSE)
